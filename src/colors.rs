bf267ccd7495ab2432d1fddef4c53e103b34e7ca
akitsu-sanae <akitsu.sanae@gmail.com>
2016-09-19 23:03:18 +0900

    add license notation

--- src/colors.rs
+++ src/colors.rs
@@ -1,3 +1,9 @@
+/*============================================================================
+  Copyright (C) 2016 akitsu sanae
+  https://github.com/akitsu-sanae/ysd
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
 
 use ncurses::*;
 use status::Mode;
